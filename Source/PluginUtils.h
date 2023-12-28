729f23f53b33ca7dee59de16d15c1cf1e62f32a4
Paul Jones <me@pljns.com>
2023-12-28 17:14:27 -0500

    Remove old commented code

--- Source/PluginUtils.h
+++ Source/PluginUtils.h
@@ -11,9 +11,4 @@ public:
 	static std::string joinId(const std::vector<std::string>& strings);
 	static bool isNumeric(const std::string& str);
 	static std::map<int, std::set<std::string>> getUniqueMidiNoteMicCombinations();
-
-	//static std::string capitalizeFirstLetter(const std::string& str);
-	//static std::string getMidiNoteParameterId(int midiNote, std::string micId, std::string param);
-	//static std::string getChannelParameterId(std::string channelName, std::string paramId);
-	//static std::string getEqualizationParameterId(std::string channelName, std::string equalitizationTypeId, std::string equalizationSubtypeId);
 };
